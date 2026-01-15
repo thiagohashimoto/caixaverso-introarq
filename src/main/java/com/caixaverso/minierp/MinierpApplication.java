68773112136806a6a7dd1d34c56d5fab6dd23a37
Thiago Hashimoto <thiago.hashimoto94@gmail.com>
2026-01-14 21:46:25 -0300

    Versão 1.0.0.1 - Commit Inicial

---
+++ src/main/java/com/caixaverso/minierp/MinierpApplication.java
@@ -0,0 +1,13 @@
+package com.caixaverso.minierp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinierpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinierpApplication.class, args);
+	}
+
+}
